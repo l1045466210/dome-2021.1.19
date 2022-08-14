1fe41309ff1c8cd42b673904172cab8caaa2ad8e
1045466210@qq.com <1045466210@qq.com>
2022-08-15 01:10:07 +0800

    第二次提交测试

--- untitled/src/com/demo/DemoText.java
+++ untitled/src/com/demo/DemoText.java
@@ -2,12 +2,13 @@ package com.demo;
 
 import java.util.Scanner;
 
-public class demoText {
+public class DemoText {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入");
         System.out.println(sc);
         System.out.println(1);
         System.out.println(2);
-   println }
+
+    }
 }
